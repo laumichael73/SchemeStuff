12c17c7cf86c004e98d88a2814202c7db9d74abf
Michael Folz <maf152@case.edu>
2018-02-19 21:54:24 -0500

    take this

--- interpreter.scm
+++ interpreter.scm
@@ -2,7 +2,7 @@
 ;Mark Gross
 
 
-;main function is called by (interpreter <string of file path and name>)
+;main function is called by (interpret "<string of file path and name>")
 ;the string is in double quotes
 
 #lang racket
@@ -29,6 +29,10 @@
 (define (m_values cstate) (cadr cstate))
 ;constructs a state from a list of vars and vals
 (define (buildstate vars vals) (list vars vals))
+
+(define (firstelement input) (car input))
+(define (secondelement input) (cadr input))
+(define (thirdelement input) (caddr input))
 
 ;adds the pair (var, val) to the state
 (define (state_append_tofront var val cstate)
@@ -62,13 +66,6 @@
           ((eq? var (car (m_state cstate))) (cons (car(m_values cstate))  (m_state_lookup var (recursestate cstate))))
           (else (m_state_lookup var (recursestate cstate)))))
 
-;cps append taken from class on 2/16
-;continuation passing style append, used in testing
-;TODO remove this
-(define (appendit l1 l2)
-  (if (null? l1) (mycont l2)
-      (cons (car l1) (appendit (cdr l1) l2))))
-(define mycont (lambda (v) v))
 
 ;------------------------------------------------------------------------------------------
 ;interpreter methods
@@ -81,94 +78,76 @@
 ;when a variable is declared, before it is used, it is associated with the value 'declared rather than a number
 ;m_state_add already checks that the variable isn't already declared
 (define (declare var cstate) (m_state_add var 'declared cstate))
+
+;=
+;checks that the value is already declared to something
+(define (equals var val cstate)
+  (cond
+    ;check that the value for the variable is 'declared or some number, ie it's already declared
+    ((not (null? (m_state_lookup var cstate))) (m_state_add var val (m_state_remove var cstate)))
+    ;if its not declared then need to declare before using
+    (else (declaringerror))))
+
+
 ;&&
 ;||
 ;%
 ;! (something)
-;=
-(define (equals var val cstate)
-  (cond
-    ((equal? (m_state_lookup var cstate) '(declared)) (m_state_add var val (m_state_remove var cstate)))
-    (else (declaringerror))))
-
-;- negation
 
 ;if
 ;else
 ;while (cond) do ()
 
-(define (interpreter filepathandname)
-  (interpret (parser filepathandname) (initialstate)))
+(define (interpret filepathandname)
+  (read (parser filepathandname) (initialstate)))
 
-(define (interpret lis cstate)
+(define (read  input cstate)
   (cond
-    ((null? lis) lis)
-    ((number? lis) lis)
-    ((null? (car lis)) (car lis))
-    ((number? (car lis)) (car lis))
-    ((list? (car lis)) (interpret (car lis) cstate) (interpret (cdr lis) cstate))
-    ;there's no third operator, find which operator it is
-    ((null? (cddr lis)) (unaryoperators lis cstate))
+    ((null?  input)  input)
+    ((number?  input)  input)
+    ((null? (firstelement  input)) (firstelement  input))
+    ((number? (firstelement  input)) (firstelement  input))
 
+    ((equal? '- (firstelement input)) (- (read (secondelement input) cstate) (read (thirdelement  input) cstate)))
+    ((equal? '/ (firstelement  input)) (floor (/ (read (secondelement input) cstate) (read (thirdelement  input) cstate))))
+    ((equal? '* (firstelement  input)) (* (read (cdr input) cstate) (read (thirdelement input) cstate)))
+    ((equal? '+ (firstelement  input)) (+ (read (cdr input) cstate) (read (thirdelement input) cstate)))
+    ((equal? '< (firstelement  input)) (< (read (cdr input) cstate) (read (thirdelement input) cstate)))
+    ((equal? '> (firstelement  input)) (> (read (cdr input) cstate) (read (thirdelement input) cstate)))
+    ((equal? '% (firstelement  input)) (modulo (read (cdr  input) cstate) (read (thirdelement  input) cstate)))
+    ;if the first statement is (var ....)
+    ((equal? 'var (firstelement  input)) (read (cdr input) (declarevariable  (firstelement input) cstate)))
+    ;if the first statement is (return ...)
+    ((equal? 'return (firstelement input)) (returnvalue input cstate))
     ;binary operators
-    ((equal? 'return (car lis)) (interpret (cdr lis) cstate))
-    ((equal? 'var (car lis)) (interpret (cdr lis) (declare (cadr lis) cstate)))
-    ((equal? '- (car lis)) (- (interpret (cadr lis) cstate) (interpret (caddr lis) cstate)))
-    ((equal? '/ (car lis)) (floor (/ (interpret (cadr lis) cstate) (interpret (caddr lis) cstate))))
-    ((equal? '* (car lis)) (* (interpret (cdr lis) cstate) (interpret (caddr lis) cstate)))
-    ((equal? '+ (car lis)) (+ (interpret (cdr lis) cstate) (interpret (caddr lis) cstate)))
-    ((equal? '< (car lis)) (< (interpret (cdr lis) cstate) (interpret (caddr lis) cstate)))
-    ((equal? '> (car lis)) (> (interpret (cdr lis) cstate) (interpret (caddr lis) cstate)))
-    ((equal? '% (car lis)) (modulo (interpret (cdr lis) cstate) (interpret (caddr lis) cstate)))
-    (else (interpret (cdr lis) cstate))))
-    
 
 
-
-
-(define (unaryoperators lis cstate)
+;returns the updated state after declaring variable, called by input
+;(car cadr caddr)
+(define (declarevariable input cstate)
   (cond
-    ((equal? 'return (car lis)) (interpret (cdr lis) cstate))
-    ((equal? '- (car lis)) (interpret (cdr lis) cstate))
-    ((equal? '! (car lis)) (interpret (cdr lis) cstate))
-    ((equal? 'var (car lis) (interpret (cdr lis) (declare (cadr lis) cstate))))
+     ;if it's a unary operator (var x)
+     ((null? (cddr input)) (declare (secondelement input) cstate))
+     ;else it's a binary (var x 10)
+     (else (equals (secondelement input) (thirdelement input) (declare (secondelement input) cstate)))))
+
+;(return <expression>)
+(define (returnvalue input cstate)
+  (cond
+    ((number? (secondelement input)) (secondelement input))
+    ((not (null? (m_state_lookup (secondelement input)) cstate)) (returnvalue (read (cdr input)) cstate))))
+  
+(define (unaryoperators  input cstate)
+  (cond
+    ((equal? 'return (firstelement  input)) (read (cdr input) cstate))
+    ((equal? '- (firstelement  input)) (read (cdr  input) cstate))
+    ((equal? '! (firstelement  input)) (read (cdr  input) cstate))
+    ((equal? 'var (firstelement  input) (read (cdr  input) (declare (secondelement  input) cstate))))
     (else (print "here"))))
 
 
 ;---------------------------------------------------------------------------------
 ;ignore this stuff, it's only for testing to make sure stuff works
 ;-----------------------------------------------------------------------------------
-(define teststate  '((x y z) (1 2 3)))
 (define nullstate '(()()))
 (define (initialstate) '(()()))
-
-
-
-(define (test1) (appendit (parser "testfiles/test1") (interpret (parser "testfiles/test1") nullstate)))
-(define (test2) (appendit (parser "testfiles/test2") (interpret (parser "testfiles/test2") nullstate))); should be -4
-(define (test3) (appendit (parser "testfiles/test3") (interpret (parser "testfiles/test3") nullstate)))
-(define (test4) (appendit (parser "testfiles/test4") (interpret (parser "testfiles/test4") nullstate)))
-(define (test5) (appendit (parser "testfiles/test5") (interpret (parser "testfiles/test5") nullstate)))
-(define (test6) (appendit (parser "testfiles/test6") (interpret (parser "testfiles/test6") nullstate)))
-(define (test7) (appendit (parser "testfiles/test7") (interpret (parser "testfiles/test7") nullstate)))
-(define (test8) (appendit (parser "testfiles/test8") (interpret (parser "testfiles/test8") nullstate)))
-(define (test9) (appendit (parser "testfiles/test9") (interpret (parser "testfiles/test9") nullstate)))
-(define (test10) (appendit (parser "testfiles/test10") (interpret (parser "testfiles/test10") nullstate) ))
-(define (test11) (appendit (parser "testfiles/test11") (interpret (parser "testfiles/test11") nullstate) ))
-(define (test12) (appendit (parser "testfiles/test12") (interpret (parser "testfiles/test12") nullstate) ))
-(define (test13) (appendit (parser "testfiles/test13") (interpret (parser "testfiles/test13") nullstate) ))
-(define (test14) (appendit (parser "testfiles/test14") (interpret (parser "testfiles/test14") nullstate) ))
-(define (test15) (appendit (parser "testfiles/test15") (interpret (parser "testfiles/test15") nullstate) ))
-(define (test16) (appendit (parser "testfiles/test16") (interpret (parser "testfiles/test16") nullstate) ))
-(define (test17) (appendit (parser "testfiles/test17") (interpret (parser "testfiles/test17") nullstate) ))
-(define (test18) (appendit (parser "testfiles/test18") (interpret (parser "testfiles/test18")nullstate) ))
-(define (test19) (appendit (parser "testfiles/test19") (interpret (parser "testfiles/test19")nullstate) ))
-(define (test20) (appendit (parser "testfiles/test20") (interpret (parser "testfiles/test20")nullstate) ))
-(define (test21) (appendit (parser "testfiles/test21") (interpret (parser "testfiles/test21")nullstate) ))
-(define (test22) (appendit (parser "testfiles/test22") (interpret (parser "testfiles/test22")nullstate) ))
-(define (test23) (appendit (parser "testfiles/test23") (interpret (parser "testfiles/test23")nullstate) ))
-(define (test24) (appendit (parser "testfiles/test24") (interpret (parser "testfiles/test24")nullstate) ))
-(define (test25) (appendit (parser "testfiles/test25") (interpret (parser "testfiles/test25")nullstate) ))
-(define (test26) (appendit (parser "testfiles/test26") (interpret (parser "testfiles/test26")nullstate) ))
-(define (test27) (appendit (parser "testfiles/test27") (interpret (parser "testfiles/test27")nullstate) ))
-(define (test28) (appendit (parser "testfiles/test28") (interpret (parser "testfiles/test28")nullstate) ))
