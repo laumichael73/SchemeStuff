b8a59789b2e37b8950dfddad57c4dad45a23c38c
Michael Folz <maf152@case.edu>
2018-03-04 20:59:28 -0500

    starting boolean evaluation

--- interpreter.scm
+++ interpreter.scm
@@ -181,8 +181,7 @@
 
 ;if it's none of those characters, throw error
 
-(define m.value.int
-  (lambda (in)
+(define (m.value.int in)
     (cond
       ((number? in) in)
       ((eq? (operator in) '+) (+ (m.value.int (operand1 in)) (operand2 in )))
@@ -190,7 +189,7 @@
       ((eq? (operator in) '*) (* (m.value.int (operand1 in) (operand2 in ))))
       ((eq? '/ (operator in)) (quotient (m.value.int (operand1 in) (m.value.int(operand2 in )))))
       ((eq? '% (operator in)) (remainder (m.value.int (operand1 in) (m.value.int (operand2 in )))))
-      (else (error "Undefined Operator")))))
+      (else (error "Undefined Operator"))))
 
 (define operator
   (lambda (e)
